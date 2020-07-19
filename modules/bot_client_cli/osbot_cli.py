ed7fe6e2204af95b8e1515d7956fb6543fd57e74
gavinnn101 <41026935+gavinnn101@users.noreply.github.com>
2020-07-19 00:33:09 -0400

    Proxy refactor
    proxy refactor for bug fixes

--- modules/bot_client_cli/osbot_cli.py
+++ modules/bot_client_cli/osbot_cli.py
@@ -5,7 +5,7 @@ import glob
 import subprocess
 try:
     from modules.helper_modules.utility import (get_user_settings, get_index,
-    get_osbot_settings)
+    read_proxy, get_osbot_settings)
 except ImportError as error:
     print(error)
 
@@ -22,29 +22,18 @@ def find_osbot():
 
     return client
 
+
 def format_current_proxy(proxy):
     """Formats and returns our current proxy for CLI use"""
     # Example returned proxy:
     # {'https': 'socks5://username:password@127.0.0.1:24613\n'}
-    proxy = str(proxy)
     proxy_auth_type = get_user_settings()[1]
-    # Formatting shenanigans to get the strings we need for CLI usage
-    if proxy_auth_type == 1: # Formatting based on user:pass@proxy:port
-        proxy_username = proxy[get_index(proxy, '/', 2)+1:get_index(proxy, ':', 3)]
-        proxy_password = proxy[get_index(proxy, ':', 3)+1:get_index(proxy, '@', 1)]
-        proxy_host = proxy[get_index(proxy, '@', 1)+1:get_index(proxy, ':', 4)]
-        proxy_port = proxy[get_index(proxy, ':', 4)+1:get_index(proxy, "'", 4)-2]
-        # Format for CLI use (ip:port:user:pass)
-        proxy = (f"{proxy_host}:{proxy_port}:{proxy_username}:{proxy_password}")
-
+    proxy_username, proxy_password, proxy_ip, proxy_port = read_proxy(proxy, proxy_auth_type)
+    if proxy_auth_type == 1:
+        proxy = (f"{proxy_ip}:{proxy_port}:{proxy_username}:{proxy_password}")
         return proxy
-
-    else: # Formatting based on proxy:port (IP authentication)
-        proxy_host = proxy[get_index(proxy, '/', 2)+1:get_index(proxy, ':', 3)]
-        proxy_port = proxy[get_index(proxy, ':', 3)+1:-4]
-        # Format for CLI use (ip:port)
-        proxy = (f"{proxy_host}:{proxy_port}")
-
+    else:
+        proxy = (f"{proxy_ip}:{proxy_port}")
         return proxy
 
 
